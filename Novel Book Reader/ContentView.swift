a4b513913f8c10b05da481d0b5b9752d5f2fd330
Victor <victor.yokoshi@gmail.com>
2025-11-12 21:11:22 +0700

    Initial Commit

---
+++ Novel Book Reader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Novel Book Reader
+//
+//  Created by Win on 12/11/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Novel Book Reader/Novel_Book_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Novel_Book_ReaderApp.swift
+//  Novel Book Reader
+//
+//  Created by Win on 12/11/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct Novel_Book_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
